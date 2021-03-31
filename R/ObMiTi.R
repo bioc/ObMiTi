9e1adb9123c20c8593a1c811cbdd688eb0847dc0
omarelashkar <o_elashkar@hotmail.com>
2021-03-31 10:30:42 +0900

    Modified after Hub upload

--- R/ObMiTi.R
+++ R/ObMiTi.R
@@ -1,11 +1,7 @@
 #' \code{ObMiTi} package
 #'
 #' Title: Ob/ob Mice Data on Normal and High Fat Diet
-#' The package provide RNA-seq count for 2 strains of mus
-#' musclus; Wild type and Ob/Ob. Each strain was divided
-#' into two groups, and each group received either chow
-#' diet or high fat diet. RNA expression was measured
-#' after 12 weeks in 7 tissues.
+#' The package provide RNA-seq count for 2 strains of mus musclus; Wild type and Ob/Ob. Each strain was divided into two groups, and each group received either chow diet or high fat diet. RNA expression was measured after 12 weeks in 7 tissues.
 #' @docType package
 #' @name ObMiTi
 #'
